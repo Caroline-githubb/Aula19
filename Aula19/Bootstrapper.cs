553e64bef6cdd8625cf81db1f4e6f3be3bd733fb
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-06-30 16:03:06 -0300

    Mesa de trabalho - Aula 19

---
+++ Aula19/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aula19.ViewModels;
+
+namespace Aula19
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            // Inicializa o projeto
+            base.Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Define qual será o Controlador padrão
+            // Nota: O controlador MainViewModel ainda não foi criado nessa etapa.
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ Aula19/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using Aula19.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula19.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        private string nomeCompleto = string.Empty;
+
+        public string NomeCompleto
+        {
+            get
+            {
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+                // Observa se o atributo/instância FirstName recebeu alguma atualização.
+                base.NotifyOfPropertyChange(() => NomeCompleto);
+            }
+        }
+    }
+}
